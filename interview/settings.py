89b6e529c6f6cd85a13df08a370725809ab1a112
Rafael Moraes <rafaelnec@gmail.com>
2021-01-04 12:00:00 -0800

    Added POST method

--- interview/settings.py
+++ interview/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = '/static/'
 
 
 TEST_RUNNER = 'interview.testrunner.NoDbTestRunner'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
